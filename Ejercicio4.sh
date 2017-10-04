7edce4a4a28ad8ed2647ddd482d0e0dbc7909c6d
Matías González Romero <31007117+mati167@users.noreply.github.com>
2017-10-04 05:42:56 -0300

    validacion espacios

--- Ejercicio4.sh
+++ Ejercicio4.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
-if [$1 = "-h"] || [$1 = "-?"] || [$1 = "-help"] ;then
+if [[$1 = "-h"]] || [[$1 = "-?"]] || [[$1 = "-help"]] ;then
 	#hacer cosas de get-help
 	exit 0
 fi
-if [ $# -lt 2 ]; then
+if [[ $# -lt 2 ]]; then
 	echo "cantidad de parametros incorrecta"
 	exit 1
-elif [ $1 = "-r" ];then
-	if [ -d $2 ]; then
+elif [[ $1 = "-r" ]];then
+	if [[$0 -gt 2]];then
+	directory="$*"
+	sed '1,3 d' directory > directory
+	else
+		directory = $2
+	if [[ -d $directory ]]; then
 		echo "Es directorio"
 		#es un directorio, hacer cosas de directorio
+		for i in *.csv; do
+			cat "$i" | awk -F "|" '{print $2}' >> nuevo.txt 
 	else
 		echo "no existe el directorio $2"
 		exit 2
 	fi
-elif [ -f $1 ]; then
-	if [ !-s $1 ]; then
+elif [[ -f $1 ]]; then
+	if [[ ! -s $1 ]]; then
 		echo "Archivo $1 Vacio"
 		exit 3
 	fi
-	if [ !-s $2 ]; then
+	if [[ ! -s $2 ]]; then
 		echo "Archivo $2 Vacio"
 		exit 4
 	fi
@@ -29,6 +36,7 @@ elif [ -f $1 ]; then
 declare -a arr=("Lunes""Martes""Miercoles""Jueves""Viernes""Sabado""Domingo")}{
 		while ($2 != ${arr[{!i}]} )
 			{i++}
+		while($2 != 
 		
 		}' $1 , $2
 fi
