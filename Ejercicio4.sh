464a94155b3cba4de85abcaee1a13db6ff142071
Matías González Romero <31007117+mati167@users.noreply.github.com>
2017-11-21 06:27:02 -0300

    TP-Bash

--- Ejercicio4.sh
+++ Ejercicio4.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+#TP N2, Ejercicio 4, entrega 2.
+#Integrantes:
+#39769558|Castillo, Tomas Eugenio
+#38325166|Gonzalez Romero, Matias
+#36808247|Oviedo, Leandro
+#39486259|Rivero, Facundo
+#40134364|Sanabria, Facundo
 if [[ "$1" == "-h" ]] || [[ "$1" == "-?" ]] || [[ "$1" == "-help" ]] ;then
-	echo "Modo de empleo 1 : ./Ejercicio4.sh ARCHIVO_VIAJE_TREN.csv ARCHIVO_RUTA.csv"
-	echo "Ejemplo : ./Ejercicio4.sh T09_07_08_retiro_tucuman.csv retiro_tucuman.csv" 	
+	echo -e "\nModo de empleo 1 : ./Ejercicio4.sh ARCHIVO_VIAJE_TREN.csv ARCHIVO_RUTA.csv"
+	echo "Ejemplo : $0 T09_07_08_retiro_tucuman.csv retiro_tucuman.csv" 	
 	echo ""
-	echo "Ejercicio4.sh comparara los horarios de llegada con los estimados e informara si se retraso o adelanto y en que ciudad, si se cumplieran todos los horarios asi lo notificara."
-	echo ""
+	echo "$0 comparara los horarios de llegada con los estimados e informara si se retraso o adelanto y en que ciudad, si se cumplieran todos los horarios asi lo notificara."
+	echo -e "\n"
 	echo "Modo de empleo 2 : ./Ejercicio4.sh -r DIRECTORIO"
 	echo ""
-	echo "Ejercicio4.sh sumara el total de pasajeros, el monto y la cantidad de viajes de cada tren de acuerdo a los archivos de viajes que se encuentren en el directorio pasado por parametro"
+	echo -e "$0 sumara el total de pasajeros, el monto y la cantidad de viajes de cada tren de acuerdo a los archivos de viajes que se encuentren en el directorio pasado por parametro\n"
 	exit 0
 fi
 if [[ $# -lt 2 ]]; then
@@ -15,42 +22,65 @@ if [[ $# -lt 2 ]]; then
 	exit 1
 fi
 if [[ $1 == "-r" ]];then
-	if [[$0 -gt 2]];then
-	dirini="$*"
-	directory="$(echo -e "{dirini}" | sed -e 's/^-r*//' | tr -d '[:space:]')"
-	else
-		directory = $2
-	fi
-	if [[ -d $directory ]]; then
+	if [[ $# -gt 3 ]];then
+		dirini="$*"
+		directorio="$(echo -e "${dirini}" | sed -e 's/^-r .\/*//')"		
+		directorio="$(echo $PWD/$directorio)"	
+else
+		directorio="$(echo $2)"
+fi
+	if [[ -d $directorio ]]; then
 		tren=01
+	mkfifo pipeEj4
 	for tren in {1..20}
 	do
 	pasajeros=0
 	monto=0
 	cantidad_viajes=0
+	contador=0
+	array=()
+	
 		if [ $tren -lt 10 ];then
-			list=$(find $1 -name "T0$tren*")
+			#list=$(find "${directorio}" -name "T0$tren*")
+			find "${directorio}" -name "T0$tren*" > pipeEj4 &
+			 while read line;do			
+				array[${contador}]=$line
+				#echo -e "$contador\t${array[${contador}]}"
+				contador=$((contador+1))
+			done < pipeEj4
 		else
-			list=$(find $1 -name "T$tren*")
+			#list=$(find "${directorio}" -name "T$tren*")
+			find "${directorio}" -name "T$tren*" > pipeEj4 &
+			 while read line;do
+				array[${contador}]=$line
+				#echo -e "$contador\t${array[${contador}]}"
+				((contador++))
+			done < pipeEj4
 		fi
-			array=(${list// / })
 		for i in "${array[@]}"
 		do
-			((cantidad_viajes++))
-			read pasajeros <<< $(echo foo | awk -F";" -v pas=$pasajeros '{pas+=$5}END{print pas}' $i )
-			read monto <<< $(echo foo | awk -F";" -v mont=$monto '{mont+=$6}END{print mont}' $i )
+			if [[ ! -z "$i" ]];then 
+				((cantidad_viajes++))
+			fi			
+			read pasajeros <<< $(echo foo | awk -F";" -v pas=$pasajeros '{pas+=$5}END{print pas}' "$(echo ${i})" )
+			read monto <<< $(echo foo | awk -F";" -v mont=$monto '{mont+=$6}END{print mont}' "$(echo ${i})" )
 		done
-		echo "TREN $tren"
+		echo -e "\nTREN $tren"
 		echo "Pasajeros = $pasajeros"
 		echo "monto = $monto"
-		echo "Cantidad de viajes = $cantidad_viajes"
+		echo -e "Cantidad de viajes = $cantidad_viajes\n"
 	done
+	rm pipeEj4
 	else
-		echo "no existe el directorio $2"
+		echo "no existe el directorio $directorio"
 		exit 2
 	fi
-fi
+	
+else
 if [[ ! -f $1 ]]; then
+	if [[ $1 == "-r" ]];then
+		exit 3		
+	fi
 	echo " $1 no es un archivo"
 	exit 3
 fi
@@ -82,25 +112,26 @@ FNR==NR{ a[$1]=$0 ; next ; bien = 1 }{
 		if( $3 == "Hora"){}
 		else{
 			if ( $2 == Linea[2]){
-			if( $3 < Linea[4]){
-				horaStr = $3
-				split( horaStr , hora , ":")
-				split(Linea[4] , hora2 , ":")
-				dif = hora2[1]*60 + hora2[2] - hora[1]*60 - hora[2]
-				print "Llego tarde a" , $1 , "por" , dif , "minutos"
-				bien = 0
+				if( $3 < Linea[4]){
+					horaStr = $3
+					split( horaStr , hora , ":")
+					split(Linea[4] , hora2 , ":")
+					dif = hora2[1]*60 + hora2[2] - hora[1]*60 - hora[2]
+					print "Llego tarde a" , $1 , "por" , dif , "minutos"
+					bien = 0
+				}
+				if( $3 > Linea[4]){
+					horaStr = $3
+					split( horaStr , hora , ":")
+					split(Linea[4] , hora2 , ":")
+					dif = hora[1]*60 + hora[2] - hora2[1]*60 - hora2[2]
+					print "Llego temprano a" , $1 , "por" , dif , "minutos"
+					bien = 0
+				}
+				if( $3 == Linea[4]){
+					print "Llego bien a la estación de" , $1
+				}
 			}
-			if( $3 > Linea[4]){
-				horaStr = $3
-				split( horaStr , hora , ":")
-				split(Linea[4] , hora2 , ":")
-				dif = hora[1]*60 + hora[2] - hora2[1]*60 - hora2[2]
-				print "Llego temprano a" , $1 , "por" , dif , "minutos"
-				bien = 0
-			}
-			if( $3 == Linea[4]){
-				print "Llego bien a la estación de" , $1
-			}}
 			else{
 					while( $2 != arr[i]){
 						i++}
@@ -120,6 +151,6 @@ FNR==NR{ a[$1]=$0 ; next ; bien = 1 }{
 			print "Cumplio todos los horarios"
 		}
 	}' $1 $2
-#fi
+fi
 
 
